bbe2659a422a0960ec5ef7072b68597a89d0fcdb
Hridoy Roy <roy.hridoy2014@gmail.com>
2019-10-26 14:38:54 +0600

    Finaly complete crud operation with api asp.net mvc 5

--- MVC/Controllers/FriendListController.cs
+++ MVC/Controllers/FriendListController.cs
@@ -19,5 +19,50 @@ namespace MVC.Controllers
             friendlist = response.Content.ReadAsAsync<IEnumerable<MVCFriendModel>>().Result;
             return View(friendlist);
         }
+
+        [HttpGet]
+        public ActionResult AddFriend(int id=0)
+        {
+            if (id == 0)
+            {
+                return View(new MVCFriendModel());
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariable.webapiclient.GetAsync("Web/"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<MVCFriendModel>().Result);
+            }
+            
+        }
+
+        [HttpPost]
+        public ActionResult AddFriend(MVCFriendModel fri)
+        {
+            if (fri.InfoID == 0)
+            {
+                HttpResponseMessage response = GlobalVariable.webapiclient.PostAsJsonAsync("Web", fri).Result;
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariable.webapiclient.PutAsJsonAsync("Web/"+fri.InfoID, fri).Result;
+            }
+            
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult DeleteFriend(int?id)
+        {
+            HttpResponseMessage response = GlobalVariable.webapiclient.GetAsync("Web/" + id.ToString()).Result;
+            return View(response.Content.ReadAsAsync<MVCFriendModel>().Result);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFriend(int id)
+        {
+            HttpResponseMessage response = GlobalVariable.webapiclient.DeleteAsync("Web/" +id.ToString()).Result;
+            return RedirectToAction("Index");
+        }
     }
 }
--- MVC/Models/MVCFriendModel.cs
+++ MVC/Models/MVCFriendModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,10 +9,15 @@ namespace MVC.Models
     public class MVCFriendModel
     {
         public int InfoID { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string MobileNumber { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string RelationshipStatus { get; set; }
     }
 }
